a20d4ad84829516fefa2db8273f107552eb37302
matanad <31176421+matanad@users.noreply.github.com>
2022-12-14 14:05:11 +0200

    Add files via upload

---
+++ js/memeController.js
@@ -0,0 +1,11 @@
+let gElCanvas
+
+function renderMeme() {
+    gElCanvas = document.querySelector('canvas')
+    gCtx = gElCanvas.getContext('2d')
+
+}
+
+function drawText(text, x, y) {
+
+}
---
+++ js/services/meme.service.js
@@ -0,0 +1,7 @@
+'use strict'
+
+var gMeme
+
+function getMeme(){
+    
+}
